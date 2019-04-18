b7a630be8d6fb58ef0febd9cc4a9927055cbca08
ApiwatHantrakool <apiwat_hantrakool@cmu.ac.th>
2019-04-18 22:02:44 +0700

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/React_Authorization/precache-manifest.f79fe7e13a63ed99862e47bc58972952.js"
+  "/RA/precache-manifest.21a5a13995ce7d36a609ef83be2075dc.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/React_Authorization/index.html", {
+workbox.routing.registerNavigationRoute("/RA/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
