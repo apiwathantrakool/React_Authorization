fb18f26a8e75e7c4dac28f0de5b0cd18920885f9
ApiwatHantrakool <apiwat_hantrakool@cmu.ac.th>
2019-04-18 21:49:30 +0700

    Updates

---
+++ precache-manifest.f79fe7e13a63ed99862e47bc58972952.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "996ab6e41770a716f0bf",
+    "url": "/React_Authorization/static/js/runtime~main.ddf37aff.js"
+  },
+  {
+    "revision": "6141ae83bb92c970a3e9",
+    "url": "/React_Authorization/static/js/main.2fb3bf4a.chunk.js"
+  },
+  {
+    "revision": "32928df91bea78d39775",
+    "url": "/React_Authorization/static/js/2.a268852a.chunk.js"
+  },
+  {
+    "revision": "6141ae83bb92c970a3e9",
+    "url": "/React_Authorization/static/css/main.5778a36f.chunk.css"
+  },
+  {
+    "revision": "32928df91bea78d39775",
+    "url": "/React_Authorization/static/css/2.78a97e69.chunk.css"
+  },
+  {
+    "revision": "b4c921f479c49d56d5c29844041ef4a8",
+    "url": "/React_Authorization/index.html"
+  }
+];
