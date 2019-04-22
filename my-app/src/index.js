a20e26be47d127b66283555daac1c64d17019fed
apiwat.h <apiwat.h@aware.co.th>
2019-04-22 17:11:41 +0700

    Add Rotation message

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -10,11 +10,12 @@ import { createStore, applyMiddleware } from "redux";
 import reducers from "./Redux/Reducers";
 import { composeWithDevTools } from "redux-devtools-extension";
 import thunk from "redux-thunk";
+import ReactOrientation from 'react-orientation';
 
 const store = createStore(
     reducers,
     composeWithDevTools(applyMiddleware(thunk))
-  );
+);
 
 ReactDOM.render(
     <Provider store={store}>
@@ -22,6 +23,16 @@ ReactDOM.render(
             <Nav />
             <App />
         </BrowserRouter>
+        <ReactOrientation type="landscape">
+            <BrowserRouter>
+                <Nav />
+                <App />
+            </BrowserRouter>
+        </ReactOrientation>
+
+        <ReactOrientation type="portrait">
+            Rotate device to portrait mode
+        </ReactOrientation>
     </Provider>
     , document.getElementById('root'));
 
